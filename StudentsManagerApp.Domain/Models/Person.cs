ad8e210eb23e94f79b640c5579963d16d513d8f9
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-09-02 19:02:57 +0300

    Добавьте файлы проекта.

---
+++ StudentsManagerApp.Domain/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using StudentsManagerApp.Domain.Common;
+using StudentsManagerApp.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManagerApp.Domain.Models
+{
+    public class Person: BaseModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public GenderType gender { get; set; }
+        public string About { get; set; }
+
+
+    }
+}
