01845907adede84bb0af694adc7d63bc394a4ecf
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-09-02 19:19:31 +0300

    Добавлена Аннотация модели данных

--- StudentsManagerApp.Domain/Common/BaseModel.cs
+++ StudentsManagerApp.Domain/Common/BaseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace StudentsManagerApp.Domain.Common
 {
     public abstract class BaseModel
     {
+        /// <summary>
+        /// Идентификатор личности
+        /// </summary>
+        [Key]
         public int Id { get; set; }
     }
 }
--- StudentsManagerApp.Domain/Models/Person.cs
+++ StudentsManagerApp.Domain/Models/Person.cs
@@ -2,6 +2,7 @@
 using StudentsManagerApp.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,36 @@ namespace StudentsManagerApp.Domain.Models
 {
     public class Person: BaseModel
     {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        [Required]
+        [StringLength(64)]
         public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        [Required]
+        [StringLength(64)]
         public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        [StringLength(64)]
+        public string LastName { get; set; } = string.Empty;
+        /// <summary>
+        /// День рождения
+        /// </summary>
         public DateTime BirthDay { get; set; }
-        public GenderType gender { get; set; }
-        public string About { get; set; }
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public GenderType Gender { get; set; }
+        /// <summary>
+        /// О личности
+        /// </summary>
+        [StringLength(65536)]
+        public string About { get; set; } = string.Empty;
 
 
     }
