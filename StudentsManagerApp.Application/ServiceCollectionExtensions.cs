b805ca22eddca7c1f8ce933dbd41429155f666c0
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-09-04 19:33:10 +0300

    Добавлен DI для Application

---
+++ StudentsManagerApp.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MediatorLib;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManagerApp.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediator(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
